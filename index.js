511af43dcd55dbe60461d8ed74dde5eb350aab30
Matt Strayer <mattstrayer77@gmail.com>
2021-08-13 09:44:31 -0600

    code cleanup

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const https = require('https');
 const bodyParser = require('body-parser');
 const { response } = require('express');
 
-
 const app = express()
-
 
 app.set('view engine', 'ejs')
 
@@ -15,8 +13,13 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"))
 
+
 const apiKey = '6c6e5e0af6715fa27517e424f5b069d2'
 
+
+// get main page - makes call to weather api for boise's weather to display when user
+// first enters page - was considering doing based on IP address but didn't like
+// allow location popup at start
 app.get('/', (req, res) => {
     
     date = getCurrentDate()
@@ -36,13 +39,13 @@ app.get('/', (req, res) => {
 })
     
 
+// takes user input and sends request to openweathermap api
+// response is parsed and formatted to be displayed in view
 app.post('/', (req, res) => {
     const location = req.body.location
-    
     const location_url = "https://api.openweathermap.org/data/2.5/forecast/daily?q="+location+"&units=imperial&cnt=8&appid=" + apiKey
-
     date = getCurrentDate()
- 
+
     https.get(location_url, (response) => {
         if (response.statusCode === 200) {
             response.on("data", (data) => {
@@ -51,18 +54,18 @@ app.post('/', (req, res) => {
                 // console.log(formatted_data)
                 res.render('index', {data: formatted_data, date: date});
             })
-            
         } else {
             res.render('index', {data: "0"})
         }
-        
     })
     
 })
 
+
+// loops through weather data forecast list and formats/converts values
+// and assigns icons
 function formatData(data) {
     for (var i=0; i < data['list'].length; i++) {
-                  
         var day = retrieveDay(data['list'][i]['dt']);
         data['list'][i]['day'] = day;
         var icon = retrieveWeatherIcon(data['list'][i]['weather'][0]['main']);
@@ -77,11 +80,14 @@ function formatData(data) {
 }
 
 
+// grabs current date formatted MM/DD/YYYY
 function getCurrentDate() {
     var today = new Date();
     var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+
     return date
 }
+
 
 // this function takes the timestamps output by the weather
 // api and converts them to readable dates
@@ -117,18 +123,21 @@ function retrieveDay(timestamp) {
 
 // function to take degrees and return text description
 function  degreeToText(degree){
-    if (degree>337.5) return 'N';
-    if (degree>292.5) return 'NW';
-    if(degree>247.5) return 'W';
-    if(degree>202.5) return 'SW';
-    if(degree>157.5) return 'S';
-    if(degree>122.5) return 'SE';
-    if(degree>67.5) return 'E';
-    if(degree>22.5){return 'NE';}
+    if (degree > 337.5) return 'N';
+    if (degree > 292.5) return 'NW';
+    if (degree > 247.5) return 'W';
+    if (degree > 202.5) return 'SW';
+    if (degree > 157.5) return 'S';
+    if (degree > 122.5) return 'SE';
+    if (degree > 67.5) return 'E';
+    if (degree > 22.5) {return 'NE';}
+
     return 'N';
 }
 
 
+// takes main weather description used as key to pull
+// svg file name (value) to match up with current weather states
 function retrieveWeatherIcon(weatherDescription) {
     var iconObject = {
         'Thunderstorm': 'thunder.svg',
@@ -139,10 +148,11 @@ function retrieveWeatherIcon(weatherDescription) {
         'Clear': 'day.svg',
         'Clouds': 'cloudy.svg'
     }
+
     return iconObject[weatherDescription]
 }
 
+
 app.listen(3000, function() {
     console.log('Running Weather app')
-
 })
